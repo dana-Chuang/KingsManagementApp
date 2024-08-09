0ee6a0a588b4cd3ed8b773d7f044d79fd536ba58
danan <danan@DANA-ROG>
2024-08-09 16:20:38 +0800

    Add project files.

---
+++ KingsManagementDB/dbo/StoredProcedures/Usp_Users_GetAll.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[Usp_Users_GetAll]
+AS
+BEGIN
+	SELECT Id,
+		CompanyCodeId,
+		EmployeeNo,
+		FirstName,
+		LastName,
+		Status,
+		Password
+	FROM [dbo].[Users];
+END
---
+++ KingsManagementDB/dbo/Tables/Users.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+    [Id] INT IDENTITY(1,1) NOT NULL, 
+    [CompanyCodeId] INT NULL, 
+    [EmployeeNo] VARCHAR(10) NOT NULL, 
+    [FirstName] VARCHAR(25) NULL, 
+    [LastName] VARCHAR(25) NULL, 
+    [Status] INT NOT NULL, 
+    [Password] VARCHAR(20) NULL,
+    [Created] DATETIME NOT NULL, 
+    [CreatedBy] CHAR(25) NOT NULL, 
+    [Updated] DATETIME NULL, 
+    [UpdatedBy] CHAR(25) NULL,
+    PRIMARY KEY CLUSTERED
+    (
+      [Id] ASC
+    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)
+    ON [PRIMARY]
+)
+
