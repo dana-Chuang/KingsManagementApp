adef4b80f19b92c7e0fb09955f148728af665a4e
Chen Ling Chuang <cchuang2@usc.edu>
2024-08-09 16:32:08 +0800

    DB(Users): add post deployment script

---
+++ KingsManagementDB/Script.PostDeployment.sql
@@ -0,0 +1,16 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Users])
+BEGIN
+    INSERT INTO [dbo].[Users] (CompanyCodeId, EmployeeNo, FirstName, LastName, Status,Password,Created, CreatedBy)
+    VALUES (0,'TestUser1','DefaultFName','DefaultLName',0,'Password',DateTime.Now,'Admin');
+END
